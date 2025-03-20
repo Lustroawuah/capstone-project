a2846fe142ab5617fe1cc19795510a0ed1fef0ee
Lustro Awuah <shineawuah@gmail.com>
2025-03-20 03:14:02 +0000

    setting up project

--- CAPSTONE-PROJECT/tailwind.config.js
+++ CAPSTONE-PROJECT/tailwind.config.js
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+<<<<<<< HEAD
 export default {
   content: [],
+=======
+module.exports ={
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // You can set it to 'media' or 'class' if needed
+>>>>>>> 05685eb (setting up project)
   theme: {
     extend: {},
   },
